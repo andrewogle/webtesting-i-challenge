ae6798e4bc58b6e757fdfacc1dfe27966c9e657a
Andrew <andrewryanogle@gmail.com>
2019-04-10 20:02:42 -0700

    finished writing functions and the repair test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,15 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('enhancer tests', ()=>{
+    describe('repair', ()=>{
+        it('should return the durability of item to 100 if less than 100', ()=>{
+            const item = {name:'cracker', enhancement: 1, durability: 20};
+            const outcome = {name: 'cracker', enhancement: 1, durability: 100};
+            
+            const test = enhancer.repair(item);
+
+            expect(test).toEqual(outcome)
+        })
+    })
+})
